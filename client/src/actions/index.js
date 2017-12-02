c28b5eb246d08543ef1668a83fb5c8c5417b1bcd
jon <bagling@gmail.com>
2017-12-02 16:57:13 +0100

    adds error handling, action types

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
+import { AUTH_USER, UNAUTH_USER, AUTH_ERROR } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -10,14 +11,24 @@ export function signinUser({ email, password }) {
         .then(response => {
           // If request is ok
           // - update state to say user is authenticated
+          // redux thunk dispatch action of type AUTH_USER
+          dispatch({ type: AUTH_USER });
           // - save jwt token
+          localStorage.setItem('token', response.data.token);
           // - redirect
           browserHistory.push('/feature');
         })
         .catch(() => {
           //  If request not ok
           // - Show error
+          dispatch(authError('Bad login info'));
         })
   }
+}
 
+export function authError(error) {
+  return {
+    type: AUTH_ERROR,
+    payload: error
+  };
 }
